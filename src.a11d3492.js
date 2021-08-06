05a6cdf62d7492849df7d3b8ef5aee7f550cba04
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-08-06 17:30:34 +0000

    Deploying to gh-pages from @ AntonTymchenko/mini-game@f2321e56a87b70e01b85e9afb3081bca20699b0c 🚀

--- src.a11d3492.js
+++ src.a11d3492.js
@@ -5,4 +5,4 @@ const t=document.getElementById("partScreen1"),e=document.getElementById("partSc
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/app.js");
 },{"./sass/main.scss":"clu1","./js/app.js":"QdeU"}]},{},["Focm"], null)
-//# sourceMappingURL=/mini-game/src.23c9abe1.js.map
+//# sourceMappingURL=/mini-game/src.a11d3492.js.map
